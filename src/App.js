7a0420269c6de1c6029f4cfe0aa45279a2047642
Rique <aguirree1985@gmail.com>
2023-11-03 14:25:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AboutMe from './components/AboutMe'
+import ContactMe from './components/ContactMe'
+import Projects from './components/Projects'
+import Tools from './components/Tools'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>Enrique</h1>
+      <AboutMe />
+      <ContactMe />
+      <Projects />
+      <Tools />
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
