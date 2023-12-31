a176a45424a92677b65c8571583d811c4f0f6db7
Rique <aguirree1985@gmail.com>
2023-12-30 16:17:53 -0800

    added movement to photo

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='about-me'>
+
+      <div> About  </div>
+ <p>    
+My name is Enrique Aguirre, and I am a student in Learn Academy's full stack web developer bootcamp. I am currently polishing my skills in JavaScript, Ruby, Ruby on Rails, Test Driven Development (TDD), HTML, CSS, Git, and PostgreSQL. My background as a United States Marine, my experience as a behavior therapist, and my extensive education in psychology have uniquely prepared me for this exciting transition. 
+</p> 
+<p>
+As a Marine Corps veteran, I bring to the table a strong foundation in discipline, dedication, and mission-driven work. As a sergeant, I supervised 15 marines in the performance of scheduled and unscheduled maintenance on weapon systems for three types of military aircraft. My leadership while deployed led to an increase in mission readiness from 60% to over 90% ensuring that air support was available for marines and other military personnel in dire situations.
+</p>
+<p>
+As a behavior therapist and educational psychologist intern, I had the privilege of helping kids with developmental disabilities meet developmental milestones. It is during this time that I began to wonder about the endless possibilities for the integration of technology into the field of applied behavior analysis. Web applications may be extremely advantageous in data collection, as tools, and as reinforcers.
+</p>
+<p>
+In addition, I hold a Master degree in Educational Psychology. This educational background has equipped me with a deep understanding of how people learn, think, and interact with technology. It's a unique perspective that I believe will be invaluable as I work to create user-friendly and engaging web applications.
+</p>
+
+Skills: JavaScript, Ruby on Rails, PostgreSQL, GitHub
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,13 @@ const Home = () => {
   return (
     <div className='HomeName'> 
         <Container className='HomeName'>
-            <h1 className='FirstNameText'>Enrique---</h1>
-            <h1 className='LastNameText'>---Aguirre</h1>
+            <h1 className='FirstNameText'>Enrique-------</h1>
+            <h1 className='LastNameText'>-------Aguirre</h1>
         </Container>
           <div id='PhotoContainer'>
+            <marquee behavior="alternate">
             <img src={proImg} alt='A very hansome guy' /> 
+            </marquee>
           </div>
           <h1 className='full-stack'>Full-Stack Web Developer</h1>
     </div>
