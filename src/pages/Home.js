14c49ff07727d4e511062b113ba1bd2de61d9e20
Rique <aguirree1985@gmail.com>
2024-02-01 20:25:54 -0800

    Adds rotation on home image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container } from 'reactstrap'
 import proImg from '../images/Rique.png'
+import '../App.css'
 const Home = () => {
   return (
     <div className='HomeName'> 
@@ -8,13 +9,12 @@ const Home = () => {
             <h1 className='FirstNameText'>Enrique-------</h1>
             <h1 className='LastNameText'>-------Aguirre</h1>
         </Container>
-          <div id='PhotoContainer'>
-            <marquee
-             behavior="alternate" direction= "left" loop="10" width= "100%" scrollamount="15" scrolldelay="1" >
-            <img src={proImg} alt='A very hansome guy' /> 
-            </marquee>
+          <div id='PhotoContainer'> 
+          <img src={proImg} alt='A very handsome guy' className='rotate' />
           </div>
-          <h1 className='full-stack'>Full-Stack Web Developer</h1>
+          <h1 className='full-stack'>Full-Stack Web Developer
+          </h1>
+          <h4 className='LastNameText'>JavaScript - React - Ruby on Rails </h4>
     </div>
   )
 }
