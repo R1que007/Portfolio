c213e215c21e1c3e8f1cec4295a172338e2d72d3
Rique <aguirree1985@gmail.com>
2024-02-09 11:54:22 -0800

    adds resposiveness to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,68 @@
-import React from 'react'
-import { Nav, NavItem } from "reactstrap";
-import { NavLink } from 'react-router-dom';
+import React, { useState} from 'react'
+import { Nav, NavItem, Collapse, Navbar, NavbarToggler } from "reactstrap"
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+    // State variable to manage visibility of resp menu
+    const [isOpen, setIsOpen] = useState(false)
     const email = "aguirree1985@gmail.com"
+    // Function to toggle the resp menu
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <div className='header'>
-            <div className='header-left'>
-                <Nav>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/" activeClassName="active-link">
-                            Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/aboutme" activeClassName="active-link">
-                            About Me
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/myprojects" activeClassName="active-link">
-                            Projects
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/contact" activeClassName="active-link">
-                            Contact Me
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </div>
-            <div>
-                <Nav>
-                    <NavItem>
-                        <a className="nav-link" href={`mailto:${email}`} aria-label="Email">
-                            {email}
-                        </a>
-                    </NavItem>
-                </Nav>
-            </div>
-        </div>
+            {/* Navbar component from Bootstrap */}
+            <Navbar expand="md">
+            {/* NavbarToggler for responsive menu */}
+            <NavbarToggler onClick={toggleNavbar} />
+
+            {/* Collapse componet to hide/show content based on screen size */}
+                <Collapse isOpen={isOpen} navbar>
+                    {/* Left-aligned navigation items */}
+                    <div className='header-left'>
+                    <Nav className="mr-auto">
+                        <NavItem>
+                            <NavLink className="nav-link" to="/" activeClassName="active-link">
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/aboutme" activeClassName="active-link-aboutme"
+                            >
+                                About Me
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/myprojects" activeClassName="active-link-myprojects"
+                            >
+                                Projects
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/contact" activeClassName="active-link-contact"
+                            >
+                                Contact Me
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    </div>
+                    {/* Right-aligned navigation items */}
+                    <div>
+                    <Nav className='ml-auto' navbar>
+                        <NavItem>
+                            <a className="nav-link" href={`mailto:${email}`} aria-label="Email">
+                                {email}
+                            </a>
+                        </NavItem>
+                    </Nav>
+                    </div>
+                </Collapse>
+            </Navbar>
+        </div >
+    
+        
     );
 }
 
